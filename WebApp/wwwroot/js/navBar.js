55cc55ef0e2c0aa1fdb0a3d1869edfa8c9ca4156
Nicolas Bertrand <nicolasbertrand2112@gmail.com>
2021-12-03 13:59:44 +0100

    Auto stash before merge of "main" and "origin/main"
    Ajout du nouveau menu du site avec le Frameworks des icons

---
+++ WebApp/wwwroot/js/navBar.js
@@ -0,0 +1,24 @@
+/**
+ * Add active class in selected list item
+ */
+let list = document.querySelectorAll('.list');
+for (let i = 0; i < list.length; i++) {
+    list[i].onclick = function () {
+        let j = 0;
+        while (j < list.length) {
+            list[j++].className = 'list';
+        }
+        list[i].className = 'list active';
+    }
+}
+/**
+ *   Toggle menu
+ */
+//let menuToggle = document.querySelector('.toogle');
+let navigation = document.querySelector('.navigation');
+let main = document.querySelector('#main');
+navigation.onclick = function () {
+    main.classList.toggle('active');
+    //menuToggle.classList.toggle('active');
+    navigation.classList.toggle('responsive');
+}
