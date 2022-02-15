91a66a67d0a6bc6edd7d79255e309107a705cb4f
Regu <leo.peyon@gmail.com>
2022-02-15 12:21:57 +0100

    Add Page

---
+++ AndroidApp/AndroidApp/Views/FavoriePage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AndroidApp.Views
+{
+    public partial class FavoriePage : ContentPage
+    {
+        public FavoriePage()
+        {
+            InitializeComponent();
+        }
+    }
+}
