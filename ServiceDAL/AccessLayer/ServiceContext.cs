f7b321e37e86e75478ab6cfe08b6495b63bd5fd7
Nicolas Bertrand <nicolasbertrand2112@gmail.com>
2022-02-20 00:45:08 +0100

    cahngemment connection a la database

--- ServiceDAL/AccessLayer/ServiceContext.cs
+++ ServiceDAL/AccessLayer/ServiceContext.cs
@@ -19,7 +19,7 @@ namespace ServiceDAL.AccessLayer
         }
 
         public ServiceContext()
-             : base("server=(localdb)\\mssqllocaldb;database=ServiceDal_Local;Trusted_Connection=True;MultipleActiveResultSets=true;")
+             : base("Server=172.16.3.1; Database=project; User Id=project ; Password=Fvo$i%3y$3Y4b6VZ; Trusted_Connection=false; MultipleActiveResultSets=true;")
         {
             SetConfiguration();
         }
